ac378556b8684be6acea45fb0c4e3e72a3952c9e
Thijs van den Hout <thijsvandenhout@live.nl>
2019-04-27 16:54:00 +0200

    remove conflicting lines in main

--- src/framework/Main.java
+++ src/framework/Main.java
@@ -33,28 +33,4 @@ public class Main {
         gm.startGame();
     }
 
-    private static void scriptedGame() {
-        Game game = new Game();
-        System.out.println(game);
-        King myKing = (King)game.getWhiteActivePieces().get(game.getWhiteActivePieces().size()-1);
-        Rook myRook = (Rook)game.getWhiteActivePieces().get(game.getWhiteActivePieces().size()-2);
-
-        myRook.move(new Position(0, 5), game.getBoard());
-
-        System.out.println(game);
-
-        System.out.println(myKing.getLegalMoves(game.getBoard()));
-
-        myKing.move(new Position(6,0), game.getBoard());
-
-        System.out.println(game);
-
-        game.undoLastMove();
-
-        System.out.println(game);
-        System.out.println(myKing.isQueenSideCastle()+" "+myKing.isKingSideCastle());
-
-    }
-
-
 }
